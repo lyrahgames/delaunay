ff89d8ab15c38d76031d93de64e99b88f047a752
lyrahgames <lyrahgames@mailbox.org>
2020-11-05 21:53:28 +0100

    Clean up Bowyer-Watson algorithm

--- delaunay/lyrahgames/delaunay/bowyer_watson.hpp
+++ delaunay/lyrahgames/delaunay/bowyer_watson.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include <algorithm>
 #include <array>
-#include <iostream>
 #include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
 //
 #include <lyrahgames/delaunay/geometry.hpp>
 
@@ -40,32 +39,26 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
        reinterpret_cast<size_t>(&bounds[2])},
   };
 
-  // const point bounds[4] = {
-  //     {-1.0e6f, -1.0e6f},
-  //     {1.0e6f, -1.0e6f},
-  //     {1.0e6f, 1.0e6f},
-  //     {-1.0e6f, 1.0e6f},
-  // };
-  // std::vector<triangle> triangles{{reinterpret_cast<size_t>(&bounds[0]),  //
-  //                                  reinterpret_cast<size_t>(&bounds[1]),  //
-  //                                  reinterpret_cast<size_t>(&bounds[2])},
-  //                                 {reinterpret_cast<size_t>(&bounds[2]),  //
-  //                                  reinterpret_cast<size_t>(&bounds[3]),  //
-  //                                  reinterpret_cast<size_t>(&bounds[0])}};
-
-  // std::unordered_map<edge, int, edge::hash> polygon{};
+  // Initialize structures for the intersection polygon.
   std::map<edge, int> polygon{};
+  // std::unordered_map<edge, int, edge::hash> polygon{};
   std::vector<size_t> bad_triangles{};
 
+  // Incrementally insert every point.
   for (const auto& p : points) {
+    // Clear the old polygon.
     polygon.clear();
     bad_triangles.clear();
 
+    // Ask for the circumcircle intersection with all triangles.
     for (size_t i = 0; i < triangles.size(); ++i) {
-      auto& t = triangles[i];
+      const auto& t = triangles[i];
       if (circumcircle_intersection(*reinterpret_cast<const point*>(t[0]),
                                     *reinterpret_cast<const point*>(t[1]),
                                     *reinterpret_cast<const point*>(t[2]), p)) {
+        // If so, mark triangle to be removed and add all edges to the polygon.
+        // Count the insertions for each edge to later be able
+        // to decide if its a boundary edge.
         bad_triangles.push_back(i);
         ++polygon[{t[0], t[1]}];
         ++polygon[{t[1], t[2]}];
@@ -73,16 +66,18 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
       }
     }
 
+    // Generate a new triangle connected to the new point for
+    // every boundary edge in the polygon.
     size_t bad_id = 0;
     auto edge_it = polygon.begin();
-
+    // There are always more triangles to be inserted than removed.
     for (; bad_id < bad_triangles.size(); ++bad_id, ++edge_it) {
       while (edge_it->second != 1) ++edge_it;
       auto& [e, i] = *edge_it;
       triangles[bad_triangles[bad_id]] =
           triangle{e[0], e[1], reinterpret_cast<size_t>(&p)};
     }
-
+    // Push back the additional triangles.
     for (; edge_it != polygon.end(); ++edge_it) {
       auto& [e, i] = *edge_it;
       if (i != 1) continue;
@@ -90,6 +85,8 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
     }
   }
 
+  // Compute the result vector of triangles by removing references
+  // to the super triangle.
   std::vector<triangle> result{};
   result.reserve(triangles.size());
   for (const auto& t : triangles) {
@@ -109,6 +106,9 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
 
 namespace experimental {
 
+// This triangulation precomputes structures for the circumcircle intersection
+// routine for every triangle and therefore speeds up the process.
+// On the other hand, more memory is needed.
 std::vector<triangle> triangulation(std::vector<point>& points) {
   // Construct regular super triangle which contains all given points.
   const auto bounds = bounding_triangle(bounding_circle(bounding_box(points)));
@@ -118,28 +118,38 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
        reinterpret_cast<size_t>(&bounds[2])},
   };
   // triangles.reserve(points.size() + 3);
+
+  // Precompute circumcircle intersection for super triangle.
   std::vector<std::array<float, 3>> cache{circumcircle_intersection_cache(
       *reinterpret_cast<const point*>(triangles[0][0]),
       *reinterpret_cast<const point*>(triangles[0][1]),
       *reinterpret_cast<const point*>(triangles[0][2]))};
   // cache.reserve(points.size() + 3);
 
-  // std::unordered_map<edge, int, edge::hash> polygon(32);
+  // Initialize structures for the intersection polygon.
   std::map<edge, int> polygon{};
+  // std::unordered_map<edge, int, edge::hash> polygon(32);
   // std::set<edge> polygon{};
   // std::unordered_set<edge, edge::hash> polygon{};
+  // std::vector<size_t> bad_triangles(32);
   std::vector<size_t> bad_triangles(32);
 
+  // Incrementally add every point.
   for (const auto& p : points) {
+    // Clear old polygon.
     polygon.clear();
     bad_triangles.clear();
 
+    // Ask for circumcircle intersection with every triangle.
     for (size_t i = 0; i < triangles.size(); ++i) {
-      auto& t = triangles[i];
+      const auto& t = triangles[i];
       if (circumcircle_intersection(*reinterpret_cast<const point*>(t[0]),
                                     cache[i], p)) {
+        // If so, mark triangle to be removed and add all edges to the polygon.
+        // Removing edges from the polygon that are no boundary edges
+        // does not improve run-time.
+        // So we stick to computing how often they were inserted.
         bad_triangles.push_back(i);
-
         ++polygon[{t[0], t[1]}];
         ++polygon[{t[1], t[2]}];
         ++polygon[{t[2], t[0]}];
@@ -158,9 +168,11 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
       }
     }
 
+    // Generate a new triangle connected to the new point for
+    // every boundary edge in the polygon.
     size_t bad_id = 0;
     auto edge_it = polygon.begin();
-
+    // There are more new triangles to insert than bad triangles.
     for (; bad_id < bad_triangles.size(); ++bad_id, ++edge_it) {
       while (edge_it->second != 1) ++edge_it;
       auto& [e, i] = *edge_it;
@@ -172,7 +184,7 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
           *reinterpret_cast<const point*>(triangles[bad_triangles[bad_id]][1]),
           *reinterpret_cast<const point*>(triangles[bad_triangles[bad_id]][2]));
     }
-
+    // Push back the additional triangles.
     for (; edge_it != polygon.end(); ++edge_it) {
       auto& [e, i] = *edge_it;
       if (i != 1) continue;
@@ -186,7 +198,7 @@ std::vector<triangle> triangulation(std::vector<point>& points) {
   }
 
   // Construct the result vector by adding all triangles
-  // not referencing points of the bounding box.
+  // not referencing points of the bounding triangle.
   std::vector<triangle> result{};
   result.reserve(triangles.size());
   for (const auto& t : triangles) {
